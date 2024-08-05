5c9cb168e659dc8a0c2931d58cc976adccc2a1f9
Andrew Chan <AndrewTziAnChan@gmail.com>
2024-08-04 22:08:17 -0400

    Update Program.cs

--- TestContainersApp/Program.cs
+++ TestContainersApp/Program.cs
@@ -5,9 +5,10 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
+        Console.WriteLine("Starting");
         Console.WriteLine(string.Join('|', args));
 
-        PostgreSqlContainer container = new PostgreSqlBuilder().Build();
+        PostgreSqlContainer container = new PostgreSqlBuilder().WithPortBinding(5432, true).Build();
         await container.StartAsync();
         
         Console.WriteLine(container.GetConnectionString());
