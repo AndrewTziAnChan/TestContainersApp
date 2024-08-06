efb384ed894c8dc2ca39617d6923396265c90208
Andrew Chan <AndrewTziAnChan@gmail.com>
2024-08-06 01:09:59 -0400

    Update Program.cs

--- TestContainersApp/Program.cs
+++ TestContainersApp/Program.cs
@@ -1,13 +1,16 @@
-﻿using DotNet.Testcontainers.Configurations;
+﻿using DotNet.Testcontainers.Builders;
 using Testcontainers.PostgreSql;
 
 public static class Program
 {
     public static async Task Main(string[] args)
     {
-        Console.WriteLine(string.Join("|", args));
-        Console.WriteLine(TestcontainersSettings.DockerHostOverride);
+        Console.WriteLine("Starting");
+        Console.WriteLine(string.Join('|', args));
+
         PostgreSqlContainer container = new PostgreSqlBuilder()
+            .WithPortBinding(5432, true)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(5432))
             .Build();
         await container.StartAsync();
 
